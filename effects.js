c99d145139c852904b5357f59b9acd0eaa76cdcf
Diogo Machado <diogo@diogomachado.com>
2016-09-21 23:26:05 -0300

    Initializing project

---
+++ effects.js
@@ -0,0 +1,26 @@
+var buttons = document.querySelectorAll("button");
+
+for(var x=0; x < buttons.length; x++){
+    buttons[x].addEventListener("click", waveEffect);
+}
+
+function waveEffect(e){
+    e.preventDefault();
+
+    var btnWidth = this.offsetWidth,
+        x = e.offsetX,
+        y = e.offsetY;
+
+    var el = this;
+    var span = document.createElement('span');
+    span.className = "effect-wave";
+
+    var wave = this.insertBefore(span, el.firstChild);
+
+    wave.style.top = y + 'px';
+    wave.style.left = x + 'px';
+
+    setTimeout(function() {
+        wave.remove();
+    }, 300);
+}
